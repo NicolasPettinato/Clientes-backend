69c9790f4d299a5335e4fcbf3a5e6939b4b3dd0d
Nicolas Pettinato <npetti87@gmail.com>
2021-07-15 14:07:13 -0300

    creación de proyecto SpringBoot

---
+++ src/main/java/com/backend/clientesapp/ClientesAppApplication.java
@@ -0,0 +1,13 @@
+package com.backend.clientesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/clientesapp/ClientesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.clientesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
