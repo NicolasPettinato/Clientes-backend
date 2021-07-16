efe197638fdba78a5780172bd015a0148267589f
Nicolas Pettinato <npetti87@gmail.com>
2021-07-16 12:01:37 -0300

    archivo sql inserts inicial

--- src/main/java/com/backend/clientesapp/controllers/ClienteRestController.java
+++ src/main/java/com/backend/clientesapp/controllers/ClienteRestController.java
@@ -4,12 +4,14 @@ import com.backend.clientesapp.models.entity.Cliente;
 import com.backend.clientesapp.models.services.ClienteService;
 import com.backend.clientesapp.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"}) //CORS
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+/* Populate tabla clientes */
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Andrés', 'Guzmán', 'profesor@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Mr. John', 'Doe', 'john.doe@gmail.com', '2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2018-01-03');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Rasmus', 'Lerdorf', 'rasmus.lerdorf@gmail.com', '2018-01-04');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Erich', 'Gamma', 'erich.gamma@gmail.com', '2018-02-01');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Richard', 'Helm', 'richard.helm@gmail.com', '2018-02-10');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Ralph', 'Johnson', 'ralph.johnson@gmail.com', '2018-02-18');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('John', 'Vlissides', 'john.vlissides@gmail.com', '2018-02-28');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Dr. James', 'Gosling', 'james.gosling@gmail.com', '2018-03-03');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Magma', 'Lee', 'magma.lee@gmail.com', '2018-03-04');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Tornado', 'Roe', 'tornado.roe@gmail.com', '2018-03-05');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Jade', 'Doe', 'jane.doe@gmail.com', '2018-03-06');
